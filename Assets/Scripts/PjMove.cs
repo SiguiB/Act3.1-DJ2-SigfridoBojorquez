011fb06d7200630f187a6f795b6a50a8697f60b7
SiguiB <99350925+SiguiB@users.noreply.github.com>
2022-09-28 04:31:15 -0600

    Act3.2
    Prefabs

--- Assets/Scripts/PjMove.cs
+++ Assets/Scripts/PjMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PjMove : MonoBehaviour
 {   
+    public GameObject BulletPrefab;
     public float Speed;
     public float JumpForce;
 
@@ -11,6 +12,7 @@ public class PjMove : MonoBehaviour
     private Animator Animator;
     private float Horizontal;
     private bool Nsuelo;
+    private float LastShoot;
 
     void Start()
     {
@@ -37,11 +39,26 @@ public class PjMove : MonoBehaviour
        {
         Jump();
        } 
+
+       if(Input.GetKeyDown(KeyCode.J) && Time.time > LastShoot + 0.25f)
+       {
+        Shoot();
+        LastShoot = Time.time;
+       }
     }
 
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up*JumpForce);
+    }
+    public void Shoot()
+    {
+        Vector3 direction;
+        if(transform.localScale.x == 1.0f) direction = Vector3.right;
+        else direction = Vector3.left;
+
+        GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        bullet.GetComponent<BulletScript>().SetDirection(direction);
     }
     private void FixedUpdate()
     {
